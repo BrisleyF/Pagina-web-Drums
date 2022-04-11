e01c8a74603f614c027548fdf85aba89f5d6a8b9
brisleyf <fajardobrisley@gmail.com>
2022-04-11 16:49:37 -0400

    Codigo JS

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,13 @@
+/* inicializacion AOS*/ 
+AOS.init();
+
+/* navbar */ 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
 });
 
+/* carousel */ 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, options);
